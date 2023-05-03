d8b7343e7b831dc4824a7330303c618ce075a0a0
Jet Braun <braun727@umn.edu>
2023-05-03 02:46:20 -0500

    Added comments + small fixes/changes + etc.

--- Scraper.py
+++ Scraper.py
@@ -1,18 +1,17 @@
-from selenium import webdriver
+#from selenium import webdriver
 import undetected_chromedriver as uc 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 
 import requests
 import time
 import sys
 
-from include import clean_search_token, scroll_to_end
+import re
 
 class Scraper:
      
     def __init__(self,
-                 chrome_driver: webdriver.Chrome,
+                 chrome_driver: uc.Chrome,
                  mode='a+'):
         self.driver = chrome_driver
         
@@ -27,46 +26,45 @@ class Scraper:
     def close_driver(self):
         self.driver.close()
 
-    def __get_line__(self,
-                     filename: str):
-        with open(filename, 'r') as infile:
-            lines = infile.read().split('\n')
-        for line in lines:
-            fields = line.split('\t')
-            if len(fields) > 1:
-                yield fields
     def scrape_navigation_page(self, 
                                output_path: str,
                                query_filter_job: str,
                                query_filter_location: str,
                                sleep_time = 0.5):
-        target_url = self.nav_url.format(
+        # Format target_url to include job & location 
+        target_nav_url = self.nav_url.format(
             job=query_filter_job,
             location=query_filter_location)
-        self.driver.get(target_url)
-        time.sleep(1)
-        self.__scroll_to_end__(
-            sleep_time=sleep_time)
-        try:
-            search_result_list = self.driver.find_element(
-                By.XPATH, '/html/body/div[1]/div/main/section[2]/ul')
-            results = search_result_list.find_elements(
-                By.TAG_NAME, 'a')
-        except: 
-            return None
-        output = {}
-        # Begin while
-        while len(results) > 0:
-            result = results.pop()
-            try:
-                span = result.find_element(By.CLASS_NAME, "sr-only").text
-                link = result.get_property('href')
+        # Load the URL
+        response = requests.get(target_nav_url)
+        if (response.status_code == 200):
+            self.driver.get(target_nav_url)
 
-                output[span] = link
-                self.__write_result_to_file__(span,
-                                          output_path=output_path)
-            except:
-                continue
+            self.__scroll_to_end__(sleep_time=sleep_time)
+            try:
+                search_result_list = self.driver.find_element(
+                    By.XPATH, '/html/body/div[1]/div/main/section[2]/ul')
+                results = search_result_list.find_elements(
+                    By.TAG_NAME, 'a')
+            except: 
+                return None
+            output = {}
+            # Begin while
+            while len(results) > 0:
+                result = results.pop()
+                try:
+                    span = result.find_element(By.CLASS_NAME, "sr-only").text
+                    link = result.get_property('href')
+
+                    output[span] = link
+
+                    self.__write_result_to_file__(span,
+                                                  output_path=output_path)
+                except:
+                    continue
+
+    # Given a filename, scrapes each URL in the file for 
+    # LinkedIn 
     def scrape_links_from_file(self,
                                input_path: str,
                                output_path: str):
@@ -105,6 +103,7 @@ class Scraper:
 
     def __scroll_to_end__(self,
                           sleep_time):
+        time.sleep(1)
         last_height = self.driver.execute_script(
             "return document.body.scrollHeight")
         while(True):
@@ -120,17 +119,22 @@ class Scraper:
             last_height = new_height
         return last_height
 
-    def scrape_url(self, url: str):
+    # Scrapes a given URL, and returns result job field 
+    # descriptors as a dict 
+    def scrape_url(self, 
+                   url: str):
         self.driver.get(url=url)
-        time.sleep(1)
         self.__scroll_to_end__(1.5) # To load all elements
         result_map = {}
         # Try to save the employer name as a value of company.
+        # There are probably better ways to do this...
         try:
             result_map["Company"] = self.driver.find_element(By.XPATH, \
               "/html/body/main/section[1]/div/section[2]/div/div[1]/div/h4/div[1]/span[1]/a").text
         except: 
             result_map["Company"] = "Not applicable"
+
+        # Tries to find descriptor box and returns None if N/A
         try:
             descriptors = self.driver.find_element(By.XPATH,\
                 "/html/body/main/section[1]/div/div/section[1]/div/ul")
@@ -143,7 +147,7 @@ class Scraper:
             # Get header as key and <span> as value.
             key = desc.find_element(By.TAG_NAME, 'h3').text
             value = desc.find_element(By.TAG_NAME, 'span').text
-
+            # Add result to current row if values are good
             if key != '' and value != '':
                 result_map[key] = value
         # Return the result map
@@ -162,6 +166,10 @@ class Scraper:
             # Write the column values
             outfile.write(','.join([value for value in result.values()]))
 
+    def __concat_list_to_row__(self,
+                               str_list: dict):
+        return ','.join([key for key in str_list.keys()])
+    
     def __log_bad_request__(self,
                             code: int):
         self.bad_request_count += 1
@@ -175,4 +183,16 @@ class Scraper:
         sys.stdout.write("\r\n{}\r\n".format(text))
         sys.stdout.flush()
 
-    
+    # Protected method to retrieve one line from 
+    # a given file
+    def __get_line__(self,
+                     filename: str):
+        with open(filename, 'r') as infile:
+            lines = infile.read().split('\n')
+        for line in lines:
+            fields = line.split('\t')
+            assert len(fields) == 3, exit("Bad fields!")
+            
+            m = re.search("https://linkedin.com/", fields[1])
+            if m:
+                yield fields
