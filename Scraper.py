5a6c9c6a5b87753b67319c7860117a2ebf1ecaf7
Jet Braun <jetbraun@gmail.com>
2024-07-28 00:23:15 -0500

    Cleaned up a few lines of code

--- Scraper.py
+++ Scraper.py
@@ -1,60 +1,90 @@
-#from selenium import webdriver
 import undetected_chromedriver as uc 
 from selenium.webdriver.common.by import By
+
+import logging
 
 import requests
 import time
 import sys
 
-import re
+def _get_line(filename: str):
+    with open(filename, 'r') as infile:
+        lines = infile.readlines()
+        for line in lines:
+            yield line.split('\t')
 
+def _scroll_to_end(driver: uc.Chrome, sleep_duration: float):
+                          
+    time.sleep(sleep_duration)
+    last_height = driver.execute_script(
+        "return document.body.scrollHeight")
+    new_height = None
+    while(new_height != last_height):
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load page
+        time.sleep(sleep_duration)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script(
+            "return document.body.scrollHeight")
+        last_height = new_height
+    return last_height
+
+def __write_result_to_file__(result: dict, 
+                             output_path: str,
+                             headers = False,
+                             mode="a+"):
+    with open(output_path, mode) as outfile:
+        # Write headers if headers == True
+        if headers: 
+            outfile.write(','.join(result.keys()))
+        # Write the column values
+        outfile.writelines(','.join(result.values()))
+
+_STATUS_ERR_MSG = "Status {code}! x{count}"
 class Scraper:
      
-    def __init__(self,
-                 chrome_driver: uc.Chrome,
-                 mode='a+'):
+    def __init__(self, chrome_driver: uc.Chrome, mode: str='a+',
+                 nav_url: str="http://www.linkedin.com/jobs/{job}-jobs-{location}"):
         self.driver = chrome_driver
         
-        self.timeout_codes = [403,429]
+        self.timeout_codes = (403, 429)
         self.bad_request_count = 0
 
         self.mode = mode
 
-        self.status_err = "Status {code}! x{count}"
-        self.nav_url = "http://www.linkedin.com/jobs/{job}-jobs-{location}"
+        self.nav_url = nav_url
 
     def close_driver(self):
         self.driver.close()
 
-    def scrape_navigation_page(self, 
-                               output_path: str,
+    def scrape_navigation_page(self, output_path: str,
                                query_filter_job: str,
                                query_filter_location: str,
                                sleep_time = 0.5):
         # Format target_url to include job & location 
-        target_nav_url = self.nav_url.format(
-            job=query_filter_job,
-            location=query_filter_location)
+        target_nav_url = self.nav_url.format(job=query_filter_job, 
+                                             location=query_filter_location)
         # Load the URL
         response = requests.get(target_nav_url)
-        if (response.status_code == 200):
+
+        if response.status_code == 200:
             self.driver.get(target_nav_url)
 
-            self.__scroll_to_end__(sleep_time=sleep_time)
+            _scroll_to_end(sleep_time=sleep_time)
             try:
-                search_result_list = self.driver.find_element(
+                search_results = self.driver.find_element(
                     By.XPATH, '/html/body/div[1]/div/main/section[2]/ul')
-                results = search_result_list.find_elements(
+                url_elems = search_results.find_elements(
                     By.TAG_NAME, 'a')
             except: 
                 return None
-            output = {}
-            # Begin while
-            while len(results) > 0:
-                result = results.pop()
+            output = dict()
+
+            for url_elem in url_elems:
                 try:
-                    span = result.find_element(By.CLASS_NAME, "sr-only").text
-                    link = result.get_property('href')
+                    span = url_elem.find_element(By.CLASS_NAME, "sr-only").text
+                    link = url_elem.get_property('href')
 
                     output[span] = link
 
@@ -70,42 +100,31 @@ class Scraper:
                                input_path: str,
                                output_path: str):
         headers = True
-        for sep_line in self.__get_line__(input_path):
-            
-            title = sep_line[0]
-            url = sep_line[1]
-            location = sep_line[2]
+        for title, url, location in _get_line(input_path):
 
             response = requests.get(url)
-            if (response.status_code == 200):   
+            if response.status_code == 200:   
                 
-                result = { 'title' : title,
-                           'location' : location,
-                           'href' : url }
+                result = {'title':title, 'location':location, 'href':url }
 
                 self.driver.get(url=url)
                 result = self.scrape_url(url)
                 if (result != None):
-                    self.__write_result_to_file__(result,
-                                                  output_path,
+                    self.__write_result_to_file__(result, output_path,
                                                   headers=headers)
                     if headers: 
                         headers = False
             else:
-                # Write message to console and sleep if needed
+                # Write message to console and sleep
                 self.__log_bad_request__(response.status_code)
-                if response.status_code in self.timeout_codes:
-                    time.sleep(15)
-                    pass
-                # Likely code 500; continue to next URL.
-                continue
+                time.sleep(15)
             # Finally close the response
             response.close()
 
     # Scrapes a given URL, and returns result job field 
     # descriptors as a dict 
-    def scrape_url(self, 
-                   url: str):
+    def scrape_url(self, url: str):
+
         self.driver.get(url=url)
         self.__scroll_to_end__(1.25) # To load all elements
         result_map = {}
@@ -124,12 +143,11 @@ class Scraper:
         except: 
             return None
         item_elems = descriptors.find_elements(By.TAG_NAME,"li")
-        # Begin inner while    
-        while len(item_elems) > 0:
-            desc = item_elems.pop()
+
+        for description in item_elems:
             # Get header as key and <span> as value.
-            key = desc.find_element(By.TAG_NAME, 'h3').text
-            value = desc.find_element(By.TAG_NAME, 'span').text
+            key = description.find_element(By.TAG_NAME, 'h3').text
+            value = description.find_element(By.TAG_NAME, 'span').text
             # Add result to current row if values are good
             if key != '' and value != '':
                 result_map[key] = value
@@ -143,54 +161,15 @@ class Scraper:
         with open(output_path, self.mode) as outfile:
             # Write headers if headers == True
             if headers: 
-                outfile.write(self.__list_to_row__(result.keys()))
-            # Write a new line
-            outfile.write('\n')
+                outfile.write(','.join(result.keys()))
             # Write the column values
-            outfile.write(self.__list_to_row__(result.values()))
+            outfile.writelines(self.__list_to_row__(result.values()))
 
-    def __list_to_row__(self,
-                               str_list: list):
-        return ','.join([key for key in str_list])
     
     def __log_bad_request__(self,
                             code: int):
         self.bad_request_count += 1
-        msg = self.status_err.format(
-            code = code,
-            count = self.bad_request_count)
-        self.__write_then_flush__(msg)
+        msg = self.status_err.format(code = code, count = self.bad_request_count)
 
-    def __write_then_flush__(self, 
-                             text : str):
-        sys.stdout.write("\r\n{}\r\n".format(text))
-        sys.stdout.flush()
+        logging.warn(msg)
 
-
-    # Protected method to retrieve one line from 
-    # a given file
-    def __get_line__(self,
-                     filename: str):
-        with open(filename, 'r') as infile:
-            lines = infile.read().split('\n')
-            for line in lines:
-                yield line.split('\t')
-
-    def __scroll_to_end__(self,
-                          sleep_time):
-                          
-        time.sleep(1)
-        last_height = self.driver.execute_script(
-            "return document.body.scrollHeight")
-        while(True):
-            self.driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);")
-            # Wait to load page
-            time.sleep(sleep_time)
-            # Calculate new scroll height and compare with last scroll height
-            new_height = self.driver.execute_script(
-                "return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-        return last_height
