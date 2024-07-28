52182e362e1496616ef9260e102ed10d7b5c1857
Jet Braun <jetbraun@gmail.com>
2024-07-28 01:40:09 -0500

    Added argparser args; cleaned up code in Scraper and refactored methods to module functions

--- Scraper.py
+++ Scraper.py
@@ -5,7 +5,8 @@ import logging
 
 import requests
 import time
-import sys
+
+from typing import Iterable
 
 def _get_line(filename: str):
     with open(filename, 'r') as infile:
@@ -16,10 +17,13 @@ def _get_line(filename: str):
 def _scroll_to_end(driver: uc.Chrome, sleep_duration: float):
                           
     time.sleep(sleep_duration)
+    
     last_height = driver.execute_script(
         "return document.body.scrollHeight")
+    
     new_height = None
     while(new_height != last_height):
+
         driver.execute_script(
             "window.scrollTo(0, document.body.scrollHeight);")
         # Wait to load page
@@ -30,22 +34,22 @@ def _scroll_to_end(driver: uc.Chrome, sleep_duration: float):
         last_height = new_height
     return last_height
 
-def __write_result_to_file__(result: dict, 
-                             output_path: str,
-                             headers = False,
-                             mode="a+"):
+def _write_result_to_file(content: Iterable, output_path: str, sep: str=",", mode: str="a+"):
+
     with open(output_path, mode) as outfile:
         # Write headers if headers == True
-        if headers: 
-            outfile.write(','.join(result.keys()))
         # Write the column values
-        outfile.writelines(','.join(result.values()))
+        outfile.writelines(sep.join(content))
+
+# Constants
 
 _STATUS_ERR_MSG = "Status {code}! x{count}"
+_MAIN_URL = "http://www.linkedin.com/jobs/{job}-jobs-{location}"
+
 class Scraper:
      
-    def __init__(self, chrome_driver: uc.Chrome, mode: str='a+',
-                 nav_url: str="http://www.linkedin.com/jobs/{job}-jobs-{location}"):
+    def __init__(self, chrome_driver: uc.Chrome, mode: str='a+',):
+
         self.driver = chrome_driver
         
         self.timeout_codes = (403, 429)
@@ -53,71 +57,81 @@ class Scraper:
 
         self.mode = mode
 
-        self.nav_url = nav_url
-
     def close_driver(self):
         self.driver.close()
 
     def scrape_navigation_page(self, output_path: str,
                                query_filter_job: str,
                                query_filter_location: str,
-                               sleep_time = 0.5):
+                               sleep_time = 0.5) -> bool:
         # Format target_url to include job & location 
-        target_nav_url = self.nav_url.format(job=query_filter_job, 
+        target_nav_url = _MAIN_URL.format(job=query_filter_job, 
                                              location=query_filter_location)
         # Load the URL
         response = requests.get(target_nav_url)
 
-        if response.status_code == 200:
-            self.driver.get(target_nav_url)
+        # Close the response and return False (failure)
+        if response.status_code != 200:
 
-            _scroll_to_end(sleep_time=sleep_time)
+            response.close()
+            return False
+
+        # Get url and call _scroll_to_end to ensure content is loaded
+        self.driver.get(target_nav_url)
+        _scroll_to_end(self.driver, sleep_time=sleep_time)
+
+        try:
+            search_results = self.driver.find_element(
+                By.XPATH, '/html/body/div[1]/div/main/section[2]/ul')
+            url_elems = search_results.find_elements(
+                By.TAG_NAME, 'a')
+        except: 
+            return False
+        
+        output = dict()
+        for url_elem in url_elems:
             try:
-                search_results = self.driver.find_element(
-                    By.XPATH, '/html/body/div[1]/div/main/section[2]/ul')
-                url_elems = search_results.find_elements(
-                    By.TAG_NAME, 'a')
-            except: 
-                return None
-            output = dict()
+                span = url_elem.find_element(By.CLASS_NAME, "sr-only").text
+                link = url_elem.get_property('href')
 
-            for url_elem in url_elems:
-                try:
-                    span = url_elem.find_element(By.CLASS_NAME, "sr-only").text
-                    link = url_elem.get_property('href')
+                output[span] = link
 
-                    output[span] = link
-
-                    self.__write_result_to_file__(span,
-                                                  output_path=output_path)
-                except:
-                    continue
+                _write_result_to_file(span,output_path=output_path)
+            except:
+                continue    
+        # Close response and return True (success)
         response.close()
+        return True
 
     # Given a filename, scrapes each URL in the file for 
     # LinkedIn 
     def scrape_links_from_file(self,
                                input_path: str,
                                output_path: str):
-        headers = True
+        # Set to true initially to write headers once.
+        write_headers = True
         for title, url, location in _get_line(input_path):
 
             response = requests.get(url)
-            if response.status_code == 200:   
-                
-                result = {'title':title, 'location':location, 'href':url }
 
-                self.driver.get(url=url)
-                result = self.scrape_url(url)
-                if (result != None):
-                    self.__write_result_to_file__(result, output_path,
-                                                  headers=headers)
-                    if headers: 
-                        headers = False
-            else:
-                # Write message to console and sleep
+            # Bad status code received, log warning to console and sleep
+            if response.status_code != 200:   
                 self.__log_bad_request__(response.status_code)
                 time.sleep(15)
+                
+            self.driver.get(url=url)
+            result = self.scrape_url(url)
+            
+            if result is None:
+                continue
+            
+            # If the headers haven't been written yet, write them and disable write_headers.
+            if write_headers:
+                _write_result_to_file(("title", "location", "href"), output_path)
+                write_headers = False
+
+            # Write job information regardless of whether headers have been written
+            _write_result_to_file((title, url, location), output_path)
             # Finally close the response
             response.close()
 
@@ -126,8 +140,11 @@ class Scraper:
     def scrape_url(self, url: str):
 
         self.driver.get(url=url)
-        self.__scroll_to_end__(1.25) # To load all elements
-        result_map = {}
+        
+        # Ensure all elements are loaded
+        _scroll_to_end(self.driver, 1.25) 
+
+        result_map = dict()
         # Try to save the employer name as a value of company.
         # There are probably better ways to do this...
         try:
@@ -142,9 +159,8 @@ class Scraper:
                 "/html/body/main/section[1]/div/div/section[1]/div/ul")
         except: 
             return None
-        item_elems = descriptors.find_elements(By.TAG_NAME,"li")
-
-        for description in item_elems:
+        
+        for description in descriptors.find_elements(By.TAG_NAME,"li"):
             # Get header as key and <span> as value.
             key = description.find_element(By.TAG_NAME, 'h3').text
             value = description.find_element(By.TAG_NAME, 'span').text
@@ -154,22 +170,11 @@ class Scraper:
         # Return the result map
         return result_map
 
-    def __write_result_to_file__(self,
-                                 result: dict, 
-                                 output_path: str,
-                                 headers = False):
-        with open(output_path, self.mode) as outfile:
-            # Write headers if headers == True
-            if headers: 
-                outfile.write(','.join(result.keys()))
-            # Write the column values
-            outfile.writelines(self.__list_to_row__(result.values()))
-
     
     def __log_bad_request__(self,
                             code: int):
         self.bad_request_count += 1
-        msg = self.status_err.format(code = code, count = self.bad_request_count)
+        msg = _STATUS_ERR_MSG.format(code = code, count = self.bad_request_count)
 
         logging.warn(msg)
 
--- main.py
+++ main.py
@@ -5,24 +5,58 @@
 import undetected_chromedriver as uc 
 from selenium.webdriver.chrome.service import Service
 
-import subprocess
-import sys, re
+import argparse
 
 from Scraper import Scraper
 
 
-def run_scrape_links(links):
-    args = ["python", "scrape_links.py", 
-            "o_links.txt", "out_done.csv"]
-    subprocess.run(args=args,)
+parser = argparse.ArgumentParser(
+    desc="This program scrapes a list of job titles and their relevant information from LinkedIn.")
+
+parser.add_argument("-j", "--job-title", type=str, required=True,
+                    help="The job title filter to apply to the LinkedIn search.")
+parser.add_argument("-l", "--location", type=str, required=True,
+                    help="The location filter to apply to the LinkedIn search.")
+
+parser.add_argument("-p", "--driver-path", type=str, required=True,
+                    help="The filepath pointing to a chromedriver.")
+
+parser.add_argument("-b", "--build-links", action="store_true", required=False,
+                    help="Specifies that the parser should build job information from specified input file.")
+
+parser.add_argument("-o", "--output-filepath", type=str, required=True,
+                    help="""If '--build-links' is disabled, then output_filepath is the list to write
+                    target URLs to. Otherwise, it's the destination to store the data from the query.""")
+parser.add_argument("-i", "--input-filepath", type=str, default=None, required=False,
+                    help="The filepath containing a list of URLs to scrape.")
 
 if __name__ == "__main__":
 
-    service = Service("Documents/chromedriver.exe")
-    driver = uc.Chrome(service=service,
-                       headless=True) 
+    args = parser.parse_args()
+
+    job_title_filter: str = args.job_title
+    job_location_filter: str = args.job_location
+
+    driver_path: str = args.driver_path
+
+    build_links: bool = args.build_links
+
+    input_path: str = args.input_filepath
+    output_path: str = args.output_filepath
+
+    service = Service(driver_path)
+    driver = uc.Chrome(service=service, headless=True) 
+
     driver.implicitly_wait(3.5)
 
     sc = Scraper(driver)
-    sc.scrape_links_from_file(input_path="o_links.txt", 
-                              output_path="o.csv")
+    
+    if build_links:
+        
+        sc.scrape_navigation_page(output_path=output_path,
+                                  query_filter_job=job_title_filter,
+                                  query_filter_location=job_location_filter)
+    
+    elif input_filepath is not None:
+
+        sc.scrape_links_from_file(input_path="o_links.txt", output_path="o.csv")
